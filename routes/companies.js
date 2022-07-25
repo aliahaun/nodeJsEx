e09f962225222d0bb5bf07d7e1d9ed873c586709
Nouhaila EL ARROUM <nouhailaelarroum99@gmail.com>
2022-07-25 11:58:36 +0100

    Adding the verification of the elements

--- routes/companies.js
+++ routes/companies.js
@@ -1,7 +1,12 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
+const Joigoose = require('joigoose')(mongoose, null, {
+    _id: false,
+    timestamps: false,
+  });
 const express = require('express');
 const router = express.Router();
+
 router.use(express.json());
 
 const type_compteN= 'Type compte';
@@ -14,85 +19,28 @@ const email_contactN= 'Email contact';
 const description_activityN= 'Description activity';
 const adressN= 'Adress';
 const zipN= 'Zip';
-
-
-
-
-const Companies = mongoose.model('companies', new mongoose.Schema({
-    domain:{
-        type: String,
-        minlength:5,
-        maxlength:100,
-    },
-    email: {
-        type: String,
-        minlength:10,
-        maxlength:100,
-    },
-    name:{
-        type: String,
-        minlength:5,
-        maxlength:50,
-    },
-    siren:{
-        type: Number
-    },
-    account_plan:{
-        type: String,
-        uppercase:true
-    },
-    adress:{
-        type: String,
-
-        minlength:8,
-        maxlength:200,
-    },
-    description_activity:{
-        type: String
-    },
-    email_contact: {
-        type: String,
-
-        minlength:10,
-        maxlength:100,
-    },
-    form_juridique:{
-        type: String,
-
-        uppercase:true
-    },
-    sector_activity:{
-        type: String,
-
-        enum:['Agroalimentaire', 'Banque / Assurance', 'Bois / Papier / Carton / Imprimerie', 'BTP / Matériaux de construction', 'Chimie / Parachimie', 'Commerce / Négoce / Distribution', 'Autre']
-    },
-    sirret:{
-        type: Number,
-        min:14
-        },
-    team_name:{
-        type: String,
-
-        min:3,
-        max:10
-    },
-    team_size:{
-        type: String,
-        
-    },
-    tva:{
-        type: Number,
-        
-    },
-    type_compte:{
-        type: String,
-        
-    },
-    zip:{
-        type: Number,
-        min:5
-    }
-}));
+var companyShema = Joi.object().keys({
+        domain: Joi.string().required()
+        ,
+        email:Joi.string().email().required(),
+        name:Joi.string().min(3).required(),
+        siren: Joi.number().required(),
+        account_plan:Joi.string().uppercase().required(),
+        adress: Joi.string().required(),
+        description_activity:Joi.string().required(),
+        email_contact:Joi.string().required(),
+        form_juridique:Joi.string().uppercase().required(),
+        sector_activity:Joi.string().required(),
+            // enum:['Agroalimentaire', 'Banque / Assurance', 'Bois / Papier / Carton / Imprimerie', 'BTP / Matériaux de construction', 'Chimie / Parachimie', 'Commerce / Négoce / Distribution', 'Autre']
+        sirret:Joi.string().required(),
+        team_name:Joi.string().required(),
+        team_size:Joi.string().required(),
+        tva:Joi.number().required(),
+        type_compte:Joi.string().required(),
+        zip:Joi.number().required()
+    });
+var ComponiesSchema = new mongoose.Schema(Joigoose.convert(companyShema));
+const Companies = mongoose.model('companies', ComponiesSchema);
 
 
 router.get('/', async (req, res) => {
@@ -101,7 +49,12 @@ router.get('/', async (req, res) => {
 } );
 
 router.post('/', async (req, res) => {
-  if (req.body.name===undefined) return res.status(400).send('error');
+    try{
+    const {error, value} = companyShema.validate(req.body)
+    if(error) {
+        return res.status(400).send(error.message);
+    }
+    
     let companies = new Companies({
         domain: req.body.domain,
         email: req.body.email,
@@ -123,6 +76,13 @@ router.post('/', async (req, res) => {
 
     companies = await companies.save();
     res.send(companies);
+} catch (err) {
+    if(err.isJoi === true){
+        err.status = 422;
+        console.log(err.message);
+    }
+    next(err);
+}
 });
 
 router.put('/:id', async (req, res) => {
@@ -157,5 +117,6 @@ router.delete('/:id', async (req, res)=> {
     if (!companies) return res.status(404).send('The company  with the given ID was not found.');
     res.send(companies);
     });
+
 
 module.exports = router;
